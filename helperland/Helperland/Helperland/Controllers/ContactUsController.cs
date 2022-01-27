06da7f344be5881303c3ab691b0ecfcbedc085b3
Yaksh Soni <yakshsoni10@gmail.com>
2022-01-27 21:37:52 +0530

    minor changes added

---
+++ helperland/Helperland/Helperland/Controllers/ContactUsController.cs
@@ -0,0 +1,28 @@
+﻿using Helperland.Data;
+using Helperland.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        private readonly HelperlandContext _helperlandContext;
+
+        public ContactUsController(HelperlandContext helperlandContext)
+        {
+            _helperlandContext = helperlandContext;
+        }
+
+        [HttpPost]
+        public IActionResult submit(ContactU contactU)
+        {
+            _helperlandContext.ContactUs.Add(contactU);
+            _helperlandContext.SaveChanges();
+            return View();
+        }
+    }
+}
--- helperland/Helperland/Helperland/wwwroot/js/script.js
+++ helperland/Helperland/Helperland/wwwroot/js/script.js
@@ -1,18 +1,32 @@
 window.addEventListener("scroll", () => {
-	let nav = document.querySelector("nav");
-	let winPos = window.scrollY > 0;
-
-	nav.classList.toggle("scroll-active", winPos);
-	btnHome.classList.toggle("b2h-btn", winPos);
+    let nav = document.querySelector("nav");
+    let winPos = window.scrollY > 0;
+    var logo_img = document.getElementById("logo-img");
+    nav.classList.toggle("scroll-active", winPos);
+    if (window.scrollY > 0) {
+        logo_img.src = "/asset/logo-small.png";
+    }
+    else {
+        logo_img.src = "/asset/logo-large.png";
+    }
 });
 
-jQuery('.ok-btn').click(function(e){
-        e.preventDefault();
-        var _this = jQuery(this);
-        _this.closest('.footer-bottom').slideUp();
+
+
+jQuery('.ok-btn').click(function (e) {
+    e.preventDefault();
+    var _this = jQuery(this);
+    _this.closest('.footer-bottom').slideUp();
 
 })
 
-$(document).on('click','ul .nav-item',function(){
-	$(this).addClass('active').siblings().removeClass('active')
+$(document).on('click', 'ul .nav-item', function () {
+    $(this).addClass('active').siblings().removeClass('active')
 })
+/*
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.nav').toggleClass("scroll-active", ($(window).scrollTop() > 0));
+    });
+});
+*/
